cad5226887135a86b5620ac814e25a6b92e3d46f
Leet <81698519+Leet-Rothschild@users.noreply.github.com>
2023-05-22 21:43:49 +0800

    part 4 video

--- renderer/js/renderer.js
+++ renderer/js/renderer.js
@@ -1,2 +1,24 @@
-const information = document.getElementById('info')
-information.innerText = `This app is using Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), and Electron (v${versions.electron()})`
+// function summarizeText() {
+//     const inputText = document.getElementById('inputText').value;
+    
+//     // Make a request to the tl;dr API using the inputText
+    
+//     // Process the response and extract the summary
+    
+//     const summaryText = 'This is the summarized text'; // Replace with the actual summarized text
+    
+//     document.getElementById('summaryText').value = summaryText;
+//   }
+
+const form = document.getElementById("inputText");
+if (form) {
+  form.onsubmit = function (e) {
+    
+    e.preventDefault();
+    const formData = new FormData(form);
+
+    for (const [key, value] of formData) {
+        console.log(`${key}: ${value}\n`); 
+    }
+  }
+}  
