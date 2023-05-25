fd2ec5e00e13464d0efd6eb946fe98e6e435ebf7
Leet <81698519+Leet-Rothschild@users.noreply.github.com>
2023-05-25 13:53:27 +0800

    Working but no Laravel App Logs Yet

--- main.js
+++ main.js
@@ -1,75 +1,302 @@
-const { app, BrowserWindow, ipcMain } = require('electron')
-const path = require('path')
+// Imported Modules
+const { app, BrowserWindow, Menu, ipcMain } = require("electron");
+const path = require("path");
 const axios = require('axios');
 const dotenv = require('dotenv').config();
+const FormData = require('form-data');
+const fs = require('fs');
 
-const isDev = true
-
-const createWindow = () => {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-        nodeIntegration: true,
-        contextIsolation: true,
-        preload: path.join(__dirname, 'preload.js'),
+// Global Variables
+const isDev = false;
+const isMac = process.platform === 'darwin';
+const template = [
+  // { role: 'appMenu' }
+  ...(isMac ? [{
+    label: app.name,
+    submenu: [
+      { role: 'about' },
+      { type: 'separator' },
+      { role: 'services' },
+      { type: 'separator' },
+      { role: 'hide' },
+      { role: 'hideOthers' },
+      { role: 'unhide' },
+      { type: 'separator' },
+      { role: 'quit' }
+    ]
+  }] : []),
+  // { role: 'fileMenu' }
+  {
+    label: 'File',
+    submenu: [
+      {
+        label: 'App Logs',
+        click: logsWindow
       },
-  })
+      {
+        label: 'About',
+        click: aboutWindow
+      },
+      isMac ? { role: 'close' } : { role: 'quit' }
+    ]
+  },
+  // { role: 'editMenu' }
+  {
+    label: 'Edit',
+    submenu: [
+      { role: 'undo' },
+      { role: 'redo' },
+      { type: 'separator' },
+      { role: 'cut' },
+      { role: 'copy' },
+      { role: 'paste' },
+      ...(isMac ? [
+        { role: 'pasteAndMatchStyle' },
+        { role: 'delete' },
+        { role: 'selectAll' },
+        { type: 'separator' },
+        {
+          label: 'Speech',
+          submenu: [
+            { role: 'startSpeaking' },
+            { role: 'stopSpeaking' }
+          ]
+        }
+      ] : [
+        { role: 'delete' },
+        { type: 'separator' },
+        { role: 'selectAll' }
+      ])
+    ]
+  },
+  // { role: 'viewMenu' }
+  {
+    label: 'View',
+    submenu: [
+      { role: 'reload' },
+      { role: 'forceReload' },
+      { type: 'separator' },
+      { role: 'resetZoom' },
+      { role: 'zoomIn' },
+      { role: 'zoomOut' },
+      { type: 'separator' },
+      { role: 'minimize' },
+      { role: 'togglefullscreen' },
+    ]
+  }
+];
+
+// Main Window
+const createWindow = () => {
+  const main = new BrowserWindow({
+    width: isDev ? 1200 : 775,
+    height: 800,
+    resizable: false,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: true,
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  const menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
 
   if (isDev) {
-    win.webContents.openDevTools()
+    main.webContents.openDevTools();
   }
 
-  win.loadFile(path.join(__dirname, './renderer/index.html'))
+  main.loadFile(path.join(__dirname, "./renderer/index.html"));
+};
+
+// Application Logs Window
+function logsWindow () {
+  const logs = new BrowserWindow({
+    width: 1200,
+    height: 500,
+    alwaysOnTop: true,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: true,
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  logs.setMenuBarVisibility(false);
+
+  if (isDev) {
+    logs.webContents.openDevTools();
+  }
+
+  logs.loadFile(path.join(__dirname, "./renderer/logs.html"));
+}
+
+// About Window
+function aboutWindow () {
+  const about = new BrowserWindow({
+    width: 400,
+    height: 400,
+    alwaysOnTop: true,
+  });
+
+  about.setMenuBarVisibility(false);
+
+  about.loadFile(path.join(__dirname, "./renderer/about.html"));
 }
 
 app.whenReady().then(() => {
+  // Initialize Functions
   ipcMain.handle('axios.openAI', openAI);
+  ipcMain.handle('axios.tesseract', tesseract);
+  ipcMain.handle('axios.supaBase', supaBase);
+  ipcMain.handle('axios.backendLaravel', backendLaravel);
+
+  // Create Main Window
   createWindow();
 
-  app.on('activate', () => {
+  // Start Window
+  app.on("activate", () => {
     if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
+      createWindow();
     }
-  })
-})
+  });
+});
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
+// Close Window
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
   }
-})
+});
 
-async function openAI(event, sentence) {
+// Main Functions
+// Axios OpenAI API
+async function openAI(event, sentence, tools_type){
+  let result = null;
+  let prompt = "";
+  let temperature = 0;
+  let max_tokens = 0;
 
-let res = null;
+  const env = dotenv.parsed;
 
-const env = dotenv.parsed;
+  switch (tools_type) {
+    case 'TLDR':
+      prompt = "Summarize the text and include a tl;dr at the start :\n\n" + sentence;
+      temperature = 0.7;
+      max_tokens = 64;
+      break;
+    case 'EssayOutline':
+      prompt = "Provide an outline for the essay on the topic:\n\n" + sentence;
+      temperature = 0.5;
+      max_tokens = 100;
+      break;
+    case 'MicroHorror':
+      prompt = "Create a 2-3 sentences horror story based on the following theme:\n\n" + sentence;
+      temperature = 0.8;
+      max_tokens = 50;
+      break;
+    case 'StudyNotes':
+      prompt = "Create study notes for the following topic:\n\n" + sentence;
+      temperature = 0.3;
+      max_tokens = 150;
+      break;
+  }
 
   await axios({
-    method: 'post',
-    url: 'https://api.openai.com/v1/completions',
-    data: {
-      model:"text-davinci-003",
-      prompt:"Input text to summarize " + sentence,
-      temperature:0.7,
-      max_tokens:60,
-      top_p:1.0,
-      frequency_penalty:0.0,
-      presence_penalty:1
-  },
-
-    headers: {
-    'Content-type':'application/json',
-    'Authorization': 'Bearer ' + env.APIKEY_openAI
-    }
+      method: 'post',
+      url: 'https://api.openai.com/v1/completions',
+      data: {
+        model: "text-davinci-003",
+        prompt: prompt,
+        temperature: temperature,
+        max_tokens: max_tokens,
+        top_p: 1.0,
+        frequency_penalty: 0.0,
+        presence_penalty: 0.0
+      },
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + env.APIKEY_OPENAI
+      }
     }).then(function (response) {
-     res =  response.data;
+      result = response.data;
     })
     .catch(function (error) {
-      res = error;
+      result = error.response.data;
+    });
+
+  return result;
+}
+// Axios Tesseract API
+async function tesseract(event, filepath){
+  let result = null;
+
+  var formData = new FormData();
+  formData.append("image", fs.createReadStream(filepath));
+
+  await axios.post('http://backend.test/api/ocr', formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
+    }).then(function (response) {
+      result = response.data;
     })
+    .catch(function (error) {
+      result = error.response.data;
+    });
 
-    return res;
-  }
+  return result;
+}
 
+// Axios Supabase API
+async function supaBase(event, method, id = '', data = null){
+  let result = null;
+  const env = dotenv.parsed;
+
+  let query = ( method == 'get' ? '?select=*' : (method == 'delete' ? '?prompt_id=eq.' + id : '') );
+  await axios({
+      method: method,
+      url: 'https://lsuibxpvxqrxhkmxcmwy.supabase.co/rest/v1/prompts' + query,
+      headers: ( method == 'post' ? {
+          'apikey': env.APIKEY_SUPABASE,
+          'Authorization': 'Bearer ' + env.APIKEY_SUPABASE,
+          'Content-Type': 'application/json',
+          'Prefer': 'return=minimal'
+        } : {
+          'apikey': env.APIKEY_SUPABASE,
+          'Authorization': 'Bearer ' + env.APIKEY_SUPABASE 
+        } ),
+      data: ( method == 'post' ? data : null )
+    }).then(function (response) {
+      result = response.data;
+    })
+    .catch(function (error) {
+      result = error.response.data;
+    });
+
+  return result;
+}
+
+// Axios Laravel API
+async function backendLaravel(event, method, path, data = null, token = ''){
+  let result = null;
+
+  await axios({
+      method: method,
+      url: 'http://backend.test/api/' + path,
+      headers: ( token == '' ? { 
+            'Accept': 'application/json',
+        } : {
+            'Accept': 'application/json',
+            'Authorization': 'Bearer ' + token
+        } ),
+      data: data
+    }).then(function (response) {
+      result = response.data;
+    })
+    .catch(function (error) {
+      result = error.response.data;
+    });
+
+  return result;
+}
